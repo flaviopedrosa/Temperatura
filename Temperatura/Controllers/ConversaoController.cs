f55ca76203a125185ab2e2a501e75eb77b294cfe
flaviopedrosa <flaviopedrosa@gmail.com>
2018-08-16 21:44:23 -0300

    Add files via upload

---
+++ Temperatura/Controllers/ConversaoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Temperatura.Controllers
+{
+    public class ConversaoController : Controller
+    {
+        [Produces("application/json")]
+        [Route("api/[controller]")]
+        [HttpGet]
+        public IActionResult Get(float celsius)
+        {
+            float fahrenheit;
+            fahrenheit = (1.8f * celsius) + 32;
+            return View(fahrenheit);
+        }
+    }
+}
